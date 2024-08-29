5d94d2d383b0f13bc40aeda56ebece9f18aac820
PaulSPuntusov <pps_ru@mail.ru>
2024-08-29 22:51:31 +0300

    стало лучше

--- src/main/java/ru/App.java
+++ src/main/java/ru/App.java
@@ -6,8 +6,19 @@ package ru;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        double str=0.;
+        double sum = 0.;
+        for (int i = 0; i < args.length; i++) {
+            try {
+                str = Double.parseDouble(args[i]);
+            } catch (NumberFormatException ex) {
+                args[i] = "0";
+            }
+            sum +=str;
+        }
+        System.out.println(sum);
+
+
     }
 }
