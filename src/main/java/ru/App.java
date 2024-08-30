e0462792f3c983422c3e13c996327e3b2f69f5d3
PaulSPuntusov <pps_ru@mail.ru>
2024-08-30 23:37:29 +0300

    все так же: в консоли работает, а в терминале - нет

--- src/main/java/ru/App.java
+++ src/main/java/ru/App.java
@@ -4,19 +4,33 @@ package ru;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main(String[] args) {
         double str=0.;
         double sum = 0.;
+
         for (int i = 0; i < args.length; i++) {
-            if(args[i]!=71){
-                str = 0.;
-            } else {
-                str = Double.parseDouble(args[i]);
-            }
-            sum +=str;
+           char[] a_arr = args[i].toCharArray();
+           for (int j = 0; j< a_arr.length; j++){
+               if(args[i].charAt(j)!=(char)48
+                       && args[i].charAt(j)!=(char)49
+                       && args[i].charAt(j)!=(char)50
+                       && args[i].charAt(j)!=(char)51
+                       && args[i].charAt(j)!=(char)52
+                       && args[i].charAt(j)!=(char)53
+                       && args[i].charAt(j)!=(char)54
+                       && args[i].charAt(j)!=(char)55
+                       && args[i].charAt(j)!=(char)56
+                       && args[i].charAt(j)!=(char)57){
+                   args[i] = "0";
+                   break;
+               }
+           }
+           str = Double.parseDouble(args[i]);
+           sum += str;
         }
         System.out.println(sum);
+
     }
 }
